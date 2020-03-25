dbd8b626a71b947ac34f9e0b6899394c34658bed
Vorcan-ui <62136978+Vorcan-ui@users.noreply.github.com>
2020-03-25 16:26:53 +0100

    Add files via upload

---
+++ src/chess/pieces/Kight.java
@@ -0,0 +1,4 @@
+package pieces;
+
+public class Kight extends Piece {
+}
--- src/chess/pieces/Piece.java
+++ src/chess/pieces/Piece.java
@@ -1,5 +1,13 @@
-package chess.pieces;
+package pieces;
 
 public abstract class Piece {
-  
+
+    private int posisionX;//położenia
+    private int posisionY;
+    private boolean available; //zmienna blokująca ruch w przypadku szacha
+    private int color; //zastanawiam się czy tutaj umieścić kolor pionka, więc wstępnie dałem
+
+    abstract public void starticPosision();//jak nazwa wskauje metoda ustawiająca położenie początkowe
+    abstract public void move();//metoda do poruszania pionkiem
+
 }

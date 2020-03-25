66218d0e157c755a2cd8b14611bfca45b6f9394a
Vorcan-ui <62136978+Vorcan-ui@users.noreply.github.com>
2020-03-25 16:37:49 +0100

    Update Piece.java

--- src/chess/pieces/Piece.java
+++ src/chess/pieces/Piece.java
@@ -4,7 +4,7 @@ public abstract class Piece {
 
     private int posisionX;//położenia
     private int posisionY;
-    private boolean available; //zmienna blokująca ruch w przypadku szacha
+    private boolean available; //zmienna blokująca ruch w przypadku szacha lub gdy pion umrze
     private int color; //zastanawiam się czy tutaj umieścić kolor pionka, więc wstępnie dałem
 
     abstract public void starticPosision();//jak nazwa wskauje metoda ustawiająca położenie początkowe

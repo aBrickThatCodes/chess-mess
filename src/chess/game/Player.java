3a1b9e0ea95711e03290a1264dac4b6519b7a71c
Vorcan-ui <62136978+Vorcan-ui@users.noreply.github.com>
2020-03-31 23:53:01 +0200

    Add files via upload

--- src/chess/game/Player.java
+++ src/chess/game/Player.java
@@ -1,5 +1,41 @@
 package chess.game;
 
-public class Player {
+public abstract class Player {
+
+    private boolean isHuman;
+    private int number;
+
+    public void setHuman(boolean isHuman){
+        this.isHuman = isHuman;
+    }
+
+    public boolean getHuman(){
+        return this.isHuman;
+    }
+
+    public void setNumber(int number){
+        this.number = number;
+    }
+
+    public int getNumber(){
+        return this.number;
+    }
+
+    public class HumanPlayer extends Player{
+
+        public HumanPlayer(int numberHuman)
+
+        {
+            this.setHuman(true);
+            this.setNumber(numberHuman);
+        }
+    }
+
+    public class AIPlayer extends Player{
+        AIPlayer(int numberComputer) {
+            this.setHuman(false);
+            this.setNumber(numberComputer);
+        }
+    }
 
 }

6d37882dc8327a9da94deb9b9ee02fad95d5e7e9
Vorcan-ui <Vorcan-ui@users.noreply.github.com>
2020-04-01 16:13:45 +0200

    Rook movment choices

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,5 +1,11 @@
 package chess.pieces;
 
+import chess.game.Spot;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class Rook extends Piece {
 
     public Rook(boolean available){
@@ -7,9 +13,48 @@ public class Rook extends Piece {
     }
 
     @Override
-    public void move(int move, int direction) {
-        // TODO Auto-generated method stub
+    public Collection<Spot> getPossibleMoves() {
 
+        List<Spot> possibleMoves = new ArrayList<Spot>();
+
+        int i=1;
+        Spot ahead = new Spot(new Spot.Location(pieceLocationSpot.location.getX(),pieceLocationSpot.location.getY()+i));
+        while(ahead.available){
+            ahead = new Spot(new Spot.Location(pieceLocationSpot.location.getX(),pieceLocationSpot.location.getY()+i)); //Spot do pierwszego dostępnego przed pionkiem
+            possibleMoves.add(ahead);
+            i++;
+        }
+
+        Spot left = new Spot(new Spot.Location(pieceLocationSpot.location.getX()-i,pieceLocationSpot.location.getY()));
+        while(left.available){
+            left = new Spot(new Spot.Location(pieceLocationSpot.location.getX()-i,pieceLocationSpot.location.getY())); //Spot do pierwszego dostępnego na lewo
+            possibleMoves.add(left);
+            i++;
+        }
+
+        Spot right = new Spot(new Spot.Location(pieceLocationSpot.location.getX()+i,pieceLocationSpot.location.getY()));
+        while(right.available){
+            right = new Spot(new Spot.Location(pieceLocationSpot.location.getX()+i,pieceLocationSpot.location.getY())); //Spot do pierwszego dostępnego na prawo
+            possibleMoves.add(right);
+            i++;
+        }
+
+        Spot behind  = new Spot(new Spot.Location(pieceLocationSpot.location.getX(),pieceLocationSpot.location.getY()-i));
+        while(behind.available){
+            behind = new Spot(new Spot.Location(pieceLocationSpot.location.getX(),pieceLocationSpot.location.getY()-i)); //Spot do pierwszego dostępnego za
+            possibleMoves.add(behind);
+            i++;
+        }
+
+        return possibleMoves;
+    }
+
+    public boolean validateMove(Spot destination) { //Sprawdzamy czy należy do zbioru
+        return this.getPossibleMoves().contains(destination);
+    }
+
+    public void move(Spot destination){ //Wykonujemy ruch
+        if (validateMove(destination)) this.pieceLocationSpot=destination;
     }
 
 }
